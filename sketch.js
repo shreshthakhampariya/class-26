2bd07d5ab1708e158f411aad9a90554482d9c681
shreshthakhampariya <shreshtha.khampariya@gmail.com>
2022-04-14 16:42:46 +0530

    added pig

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ function setup(){
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
-
+    pig4 = new Pig(910,220);
     log3 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
@@ -53,6 +53,7 @@ function draw(){
     box3.display();
     box4.display();
     pig3.display();
+    pig4.display();
     log3.display();
 
     box5.display();
